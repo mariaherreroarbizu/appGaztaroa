91c3c64980dd899a1c7edc64dc8b482f28effb32
mariaherreroarbizu <mherreroarbizu@gmail.com>
2025-04-08 21:02:35 +0200

    Primeros pasos en React Native

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b3e5fc', //cambio a azul el fondo
     alignItems: 'center',
     justifyContent: 'center',
   },
